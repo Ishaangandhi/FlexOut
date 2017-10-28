98fa4d8cd7928fcd81258f91a64b77dc725d2850
Ishaan Gandhi <ishaangandhi@gmail.com>
2017-10-27 20:30:20 -0700

    Initial commit

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+from pathlib import Path
+import os.path, json
+
+app = Flask(__name__)
+
+@app.route("/")
+def landing():
+    return "Flex."
+    
+def build_prices():
+    price_dict = {}
+    with open('prices.txt', 'r') as json_prices_file:
+        for line in json_prices_file:
+            [item,price] = line.split('\t')
+            price_dict[item] = price.rstrip()
+            
+    json_file = open("prices.json","w+")
+    json_file.write(json.dumps(price_dict, sort_keys=True,
+        indent=4, separators=(',', ': ')))
+    json_file.close()
+        
+
+@app.route('/prices', methods=['GET', 'POST'])
+def prices():
+    json_prices = Path("prices.json")
+    if not json_prices.is_file():
+        build_prices()
+        
+    with open('prices.json', 'r') as json_prices_file:
+        json_content = json_prices_file.read()
+        return json_content
